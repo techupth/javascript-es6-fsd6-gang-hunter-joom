b5e00ec93f1624a163f6c2ce221b78f743530402
Joom <jnwongsa@gmail.com>
2024-05-24 16:21:03 +0700

    update:ex1-7

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,10 @@
-function sum() {
+function sum(...numbers) {
   // Start coding here !
+    let total = 0
+    for(let number of numbers) {
+        total += number;
+    }
+    return total;
 }
 
 let result1 = sum(1, 2, 3, 4, 5, 6, 7, 8);
--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,9 @@
 let height = undefined;
 let result;
 
+function checkHeight (height) {
+    return height = height ?? "Height is not defined";
+    
+}
+
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
